05440a22862d2eb4ad5856e891b971b160e18564
Dylan Tompkins <dltompki@outlook.com>
2023-12-08 20:52:45 -0800

    nvim snippets, formatting. zathura config. hyprland config.

--- home.nix
+++ home.nix
@@ -1,10 +1,13 @@
-{ config, lib, pkgs, ... }:
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   nixvim = import (builtins.fetchGit {
     url = "https://github.com/nix-community/nixvim";
   });
-in
-{
+in {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "dylan";
@@ -82,10 +85,14 @@ in
     '';
   };
 
-  imports = [ nixvim.homeManagerModules.nixvim ];
+  imports = [nixvim.homeManagerModules.nixvim];
 
   programs.nixvim = {
     enable = true;
+
+    extraPackages = with pkgs; [
+      alejandra
+    ];
 
     extraPlugins = with pkgs.vimPlugins; [
       everforest
@@ -109,10 +116,10 @@ in
       nvim-cmp = {
         enable = true;
         sources = [
-          { name = "nvim_lsp"; }
-          { name = "buffer"; }
-          { name = "path"; }
-          { name = "luasnip"; }
+          {name = "nvim_lsp";}
+          {name = "buffer";}
+          {name = "path";}
+          {name = "luasnip";}
         ];
         mapping = {
           "<CR>" = "cmp.mapping.confirm({ select = true })";
@@ -143,12 +150,22 @@ in
       };
       luasnip = {
         enable = true;
-        fromVscode = [{ }];
+        fromVscode = [{}];
       };
-      lsp-format.enable = true;
       neo-tree.enable = true;
       which-key.enable = true;
       lualine.enable = true;
+      conform-nvim = {
+        enable = true;
+        formattersByFt = {
+          tex = ["latexindent"];
+          nix = ["alejandra"];
+        };
+        formatOnSave = {
+          timeoutMs = 500;
+          lspFallback = true;
+        };
+      };
     };
 
     options = {
@@ -159,6 +176,187 @@ in
       expandtab = true;
       fillchars.eob = " ";
       ignorecase = true;
+    };
+  };
+
+  programs.zathura = {
+    enable = true;
+    options = {
+      selection-clipboard = "clipboard";
+    };
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    enableNvidiaPatches = true;
+    settings = {
+      monitor = ",preferred,auto,auto";
+
+      # unscale xwayland to make not pixelated
+      xwayland = {
+        force_zero_scaling = true;
+      };
+
+      env = [
+        # independent toolkit scaling vars
+        "GDK_SCALE,2"
+        "XCURSOR_SIZE,24"
+        # from nvidia section of hyprland wiki
+        "LIBVA_DRIVER_NAME,nvidia"
+        "XDG_SESSION_TYPE,wayland"
+        "GBM_BACKEND,nvidia-drm"
+        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+        "WLR_NO_HARDWARE_CURSORS,1"
+      ];
+
+      exec-once = [
+        "waybar &"
+        "hyprpaper &"
+        "dunst &"
+        "/usr/lib/polkit-kde-authentication-agent-1"
+      ];
+
+      input = {
+        kb_layout = "us";
+
+        follow_mouse = 1;
+
+        touchpad = {
+          natural_scroll = "no";
+          scroll_factor = 0.2;
+        };
+
+        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+      };
+
+      general = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+        gaps_in = 5;
+        gaps_out = 20;
+        border_size = 2;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+
+        layout = "dwindle";
+      };
+
+      decoration = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+        rounding = 10;
+
+        drop_shadow = "yes";
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
+
+      animations = {
+        enabled = "yes";
+
+        # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      dwindle = {
+        # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+        pseudotile = "yes"; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        preserve_split = "yes"; # you probably want this
+      };
+
+      master = {
+        # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+        new_is_master = true;
+      };
+
+      gestures = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        workspace_swipe = "off";
+      };
+
+      misc = {
+        disable_hyprland_logo = true;
+      };
+
+      # Example per-device config
+      # See https://wiki.hyprland.org/Configuring/Keywords/#executing for more
+      #"device:epic-mouse-v1 = {
+      #    sensitivity = -0.5;
+      #};
+
+      # Example windowrule v1
+      # windowrule = float, ^(kitty)$
+      # Example windowrule v2
+      # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
+      # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
+
+      # See https://wiki.hyprland.org/Configuring/Keywords/ for more
+      "$mainMod" = "SUPER";
+
+      # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
+      bind = [
+        "$mainMod, Q, exec, kitty"
+        "$mainMod, C, killactive, "
+        "$mainMod, M, exit, "
+        "$mainMod, E, exec, dolphin"
+        "$mainMod, V, togglefloating, "
+        "$mainMod, R, exec, wofi --show drun --normal-window"
+        "$mainMod, L, exec, swaylock"
+        "$mainMod, P, pseudo, # dwindle"
+        "$mainMod, J, togglesplit, # dwindle"
+
+        # Move focus with mainMod + arrow keys
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+
+        # Switch workspaces with mainMod + [0-9]
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+        # Scroll through existing workspaces with mainMod + scroll
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
+      ];
+
+      # Move/resize windows with mainMod + LMB/RMB and dragging
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
     };
   };
 }
