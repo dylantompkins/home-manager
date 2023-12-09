c2782fa924b6bdc065f13f961b9fd71b9c4ffe5a
Dylan Tompkins <dltompki@outlook.com>
2023-12-09 13:06:29 -0800

    feat: update active window border gradient to use everforest colors

--- home.nix
+++ home.nix
@@ -236,7 +236,7 @@ in {
         gaps_in = 5;
         gaps_out = 20;
         border_size = 2;
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.active_border" = "rgba(7FBBB3ee) rgba(83C092ee) 45deg";
         "col.inactive_border" = "rgba(595959aa)";
 
         layout = "dwindle";
